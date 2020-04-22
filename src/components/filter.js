fda188467bfc00b31bfcf6743ebcc3d0480436d7
Irina Mateush <ismateush@yandex-team.ru>
2020-04-22 23:29:53 +0300

    Структуры данных

--- src/components/filter.js
+++ src/components/filter.js
@@ -18,9 +18,7 @@ const createFilterMarkup = (filter, isChecked) => {
 export const createFilterTemplate = (filters) => {
   const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
 
-  return (
-    `<section class="main__filter filter container">
-      ${filtersMarkup}
-    </section>`
-  );
+  return `<section class="main__filter filter container">
+    ${filtersMarkup}
+  </section>`;
 };
